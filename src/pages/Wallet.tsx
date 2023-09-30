570b8feff14c3cc24ced487426df5428344aa788
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-09-30 13:30:13 -0300

    Header

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
 
 function Wallet() {
   return (
     <div>
       <Header />
+      <WalletForm />
     </div>
   );
 }
