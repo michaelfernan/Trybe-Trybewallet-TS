6c4051800eae18072882719f4d52748c45715660
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-10-01 13:04:50 -0300

    header

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../Type';
+
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
 import ExpenseTable from '../components/Table';
-import { addExpense } from '../redux/actions';
 
 function Wallet() {
-  const currencies = useSelector((state: RootState) => state.wallet.currencies);
-  const dispatch = useDispatch();
-
-  const handleAddExpense = (expense: any) => {
-    dispatch(addExpense(expense));
-  };
-
   return (
     <div>
       <Header />
